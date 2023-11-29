f37e3a737229a5ef3e6b7ced0116871e4c2581ca
AylRoseOfficial <72778038+AylRoseOfficial@users.noreply.github.com>
2023-11-29 01:45:08 -0600

    RelayCommand-RF

--- Commands/RelayCommand.cs
+++ Commands/RelayCommand.cs
@@ -4,18 +4,20 @@ using System.Windows.Input;
 namespace RoseOrganizer.Commands {
     public class RelayCommand : ICommand {
 
-
+        // Event Handler
         public event EventHandler CanExecuteChanged;
+        
+        // Action & Predicate
         private Action<object> _execute { get; set; }
         private Predicate<object> _canExecute { get; set; }
 
-        /// Commando de Relay
-        public RelayCommand(Action<object> ExecuteMethod, Predicate<object> CanExecuteMethod) { 
-            _execute = ExecuteMethod;
-            _canExecute = CanExecuteMethod;
-        }
-
+        // Bool and Void Execute
         public bool CanExecute(object parameter) { return _canExecute(parameter); }
         public void Execute(object parameter) { _execute(parameter); }
+        
+        // RelayCommand Function Action
+        public RelayCommand(Action<object> ExecuteMethod, Predicate<object> CanExecuteMethod) { 
+            _execute = ExecuteMethod; _canExecute = CanExecuteMethod;
+        }
     }
 }
